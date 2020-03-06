36d6a621b6ef479fd1c4a7d8e9ad635a1d1bbef6
Fede Pacher <fede_pacher@hotmail.com>
2020-03-06 17:13:51 -0300

    agreagado de sentido hacia adelante y hacia atras de los leds segun pulsador 1

--- Trabajo1/Ejercicio1_V1/inc/key.h
+++ Trabajo1/Ejercicio1_V1/inc/key.h
@@ -24,8 +24,21 @@ extern "C" {
 /*==================[external data declaration]==============================*/
 
 /*==================[external functions definition]==========================*/
+/**	@brief	Rutina que inicializa un arreglo de pulsadores
+ *
+ */
 void key_init(void);
+
+/**	@brief	Rutina que indica si un boton fue pulsado
+ *
+ * 	@param[in]	id 		posicion en el vector contenedor de pulsadores
+ *
+ */
 bool key_getPressEv(board_sw_id_enum id);
+
+/**	@brief	Rutina que se chequea periodicamente cada 1ms para determinar si se produjo un evento en los pulsadores
+ *
+ */
 void key_periodicTask1ms(void);
 
 
--- Trabajo1/Ejercicio1_V1/src/app.c
+++ Trabajo1/Ejercicio1_V1/src/app.c
@@ -15,12 +15,21 @@
 #define DUTY_CICLE 200
 
 /*=====[Encabezados de funciones]=============================================*/
-void Prender_Leds(uint8_t led);
-void Apagar_Leds();
+
 
 
 /*=====[Typedef]==============================================================*/\
+typedef enum{
+	PARADA = 0,
+	ADELANTE,
+	ATRAS,
+	TOTAL_ESTADOS
+}estado_secuencia;
 
+typedef enum{
+	APAGAR,
+	PRENDER
+}estado_led;
 
 
 const uint8_t cantidad_leds = 3;	//indica la cantidad de led disponibles
@@ -35,53 +44,95 @@ int main( void )
 
    key_init();		//inicializacion del arreglo contenedor de los pulsadores a FALSE, es decir, no pulsados
 
-   /*uint16_t duty_cicle = 0;		//variable que almacena el tiempo apagado/perndido de los leds
-   bool_t estado_led = FALSE;	//variable que indica el estado del led TRUE = prender, FALSE = apagar
-   int8_t led_activo = 0;
-   bool_t sentido_giro = 1;		//indica el sentido de giro de los leds; 0 = L3 -> L2 -> L1
-   */
+   uint16_t duty_cicle = 0;		//variable que almacena el tiempo apagado/perndido de los leds
+   estado_secuencia sentido = PARADA;		//indica el sentido de giro de los leds; 0 = L3 -> L2 -> L1
+   estado_secuencia sentido_anterior = sentido;	//indica el sentido anterior
+   int8_t led_encender = 0;		//indica que led tiene que encender en la secuencia, le puse int8_t porque cuando invierte sentido llega a un valor negativo
+
+   estado_led estado = PRENDER;		//indica si debe proceder con el avance al proximo led
+
    // ---------- REPETIR POR SIEMPRE --------------------------
+
+
    while( TRUE ) {
 
+	   duty_cicle++;
 	   key_periodicTask1ms();		//chequeo por pooling si un pulsador fue presionado
 
-	   if(key_getPressEv(BOARD_SW_ID_1))
-		   board_setLed(BOARD_LED_ID_1, BOARD_LED_MSG_TOGGLE);
+	   if(key_getPressEv(BOARD_SW_ID_1)){	//si se pulsa el sw1 comienza secuencia para un lado
+		   sentido++;
+		   if(sentido >= TOTAL_ESTADOS)
+			   sentido = PARADA;
+	   }
 
-
-	   if(key_getPressEv(BOARD_SW_ID_2))
-	  		   board_setLed(BOARD_LED_ID_2, BOARD_LED_MSG_TOGGLE);
-
-	   if(key_getPressEv(BOARD_SW_ID_3))
-	  	  		   board_setLed(BOARD_LED_ID_3, BOARD_LED_MSG_TOGGLE);
-
-	   /*duty_cicle++;
-
-	   if(duty_cicle >= DUTY_CICLE){
+	   switch(sentido)
+	   {
+	   case PARADA:		//limpia todos los leds
 		   duty_cicle = 0;
-		   if (estado_led){
-			   Prender_Leds(led_activo);
-			   if(sentido_giro == 0){
-				   led_activo++;
-			   }
-			   else{
-				   led_activo--;
-			   }
+		   if(sentido != sentido_anterior){
+			   sentido_anterior = sentido;
+			   int i = 0;
+		   	   for( i = 0; i <= BOARD_LED_ID_TOTAL; i++){
+			   	   board_setLed(i, BOARD_LED_MSG_OFF);
+		   	   }
 		   }
-		   else{
-			   Apagar_Leds();
+		   break;
+	   case	ADELANTE:	//sentido hacia adelante
+		   sentido_anterior = sentido;
+		   if(duty_cicle >= DUTY_CICLE){
+			   duty_cicle = 0;
+
+			   switch(estado){
+			   case	APAGAR:
+				   board_setLed(led_encender, BOARD_LED_MSG_OFF);
+				   led_encender++;
+				   if(led_encender >= BOARD_LED_ID_TOTAL){
+					   led_encender = BOARD_LED_ID_1;
+				   }
+				   estado = PRENDER;
+				   break;
+			   case PRENDER:
+				   board_setLed(led_encender, BOARD_LED_MSG_ON);
+				   estado = APAGAR;
+				   break;
+			   default:
+				   break;
+
+			   }
 		   }
 
-		   if(led_activo >= cantidad_leds){
-			   led_activo = 0;
-		   }
-		   else{
-			   if(led_activo <0)
-				   led_activo = cantidad_leds;
-		   }
+		   break;
 
-		   estado_led = !estado_led;
-	   }*/
+	   case ATRAS:		//sentido hacia atras
+		   sentido_anterior = sentido;
+		   if(duty_cicle >= DUTY_CICLE){
+			   duty_cicle = 0;
+
+			   switch(estado){
+			   case	APAGAR:
+				   board_setLed(led_encender, BOARD_LED_MSG_OFF);
+				   led_encender--;
+				   if(led_encender < BOARD_LED_ID_1){
+					   led_encender = BOARD_LED_ID_TOTAL - 1;
+				   }
+				   estado = PRENDER;
+				   break;
+			   case PRENDER:
+				   board_setLed(led_encender, BOARD_LED_MSG_ON);
+				   estado = APAGAR;
+				   break;
+			   default:
+				   break;
+
+			   }
+		   }
+		   break;
+	   default:
+
+		   break;
+
+	   }
+
 
 	   delay(1);
 
@@ -97,28 +148,5 @@ int main( void )
  * 	@param[in]	led	Referencia al led a prender *
  */
 
-void Prender_Leds(uint8_t led)
-{
-	switch(led){
-	case 0:
-		gpioToggle(LED1);
-		break;
-	case 1:
-		gpioToggle(LED2);
-		break;
-	case 2:
-		gpioToggle(LED3);
-		break;
-	}
-
-}
-
-/**	@brief	Funcion que produce el apagado de todos los leds *
- */
-void Apagar_Leds(){
-	gpioWrite( LED1, OFF );
-	gpioWrite( LED2, OFF );
-	gpioWrite( LED3, OFF );
-}
 
 

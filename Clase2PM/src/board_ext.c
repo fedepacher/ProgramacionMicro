11e4a269fe6c959892b5c83ebeaece23232857ed
Fede Pacher <fede_pacher@hotmail.com>
2020-03-11 20:31:51 -0300

    agregado de carpeta Clase2PM correspondiente a clase 2 de ProgMic con programa base prender led1 con sw1

---
+++ Clase2PM/src/board_ext.c
@@ -0,0 +1,47 @@
+/*
+ * board_ext.c
+ *
+ *  Created on: Mar 6, 2020
+ *      Author: fedepacher
+ */
+
+#include "../../Clase2PM/inc/board_ext.h"		// su propia biblioteca
+
+#include "sapi.h"        // <= Biblioteca sAPI
+
+/*=================================[Declaraciones internas]=====================================*/
+static const gpioMap_t board_gpioLeds[] = {
+		LED1, LED2, LED3
+};
+
+
+static const gpioMap_t board_gpioSw[] = {
+		TEC1, TEC2, TEC3, TEC4
+};
+
+
+void board_setLed(board_led_id_enum id, board_led_msg_enum msg){
+
+	switch(msg){
+		case BOARD_LED_MSG_OFF:
+			gpioWrite(board_gpioLeds[id], OFF);
+		break;
+
+		case BOARD_LED_MSG_ON:
+			gpioWrite(board_gpioLeds[id], ON);
+			break;
+
+		case BOARD_LED_MSG_TOGGLE:
+			gpioToggle(board_gpioLeds[id]);
+			break;
+
+		default:
+			break;
+	}
+}
+
+bool_t board_getSw(board_sw_id_enum id){
+
+	return !gpioRead( board_gpioSw[id] );
+}
+

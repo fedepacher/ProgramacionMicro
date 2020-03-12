8b617a4af50fe80dafd04ad66af810af32a4c138
Fede Pacher <fede_pacher@hotmail.com>
2020-03-11 23:32:37 -0300

    Clase2PM ejemplo de uso de delay no bloqueante

--- Clase2PM/enunciado1.md
+++ Clase2PM/enunciado1.md
@@ -1,26 +1,7 @@
-PdM - Ejercicios clase 1
+Clase 2 Programacion de Microcontroladores 11CO2020
 
-Ejercicio 1 
+leer Uso de printf y scanf que esta en la carpeta example/c/newlib_printf_scanf
 
-Objetivo
-Practicar la utilización del lenguaje C y el desarrollo de programas en microcontroladores. En particular utilizar las funciones de manejo de GPIO y retardos bloqueantes (Delays).
+Utilizacion de delay no bloqueante
 
-Resumen
-Implementar un programa que controle el sentido en que se encienden consecutivamente los LEDs de la EDU-CIAA-NXP, mediante un pulsadores.
-Condiciones de funcionamiento
-- Utilizar solamente los leds LED1, LED2 y LED3.
-- Inicialmente, la secuencia es: LED1 -> LED2 -> LED3 -> LED1 ...
-- El período de encendido de cada led debe ser 1 seg con duty 50%.
-- Una vez que se pasa al siguiente led los demás deberán apagarse.
-- Controlar el sentido de la secuencia con la tecla TEC1.  Cada vez que se presione la TEC1 se debe invertir el sentido de la secuencia. Las secuencias posibles son dos:
-LED1 -> LED2 -> LED3 -> LED1 ...
-LED1 -> LED3 -> LED2 -> LED1 ...
-
-
-Para pensar al finalizar el ejercicio:
-
-1-¿Cuán sencillo es agregar un nuevo led a la secuencia, supongamos el LEDB?
-2-¿Se puede cambiar el sentido de la secuencia en cualquier momento de la ejecución  o se debe esperar algún momento en especial? ¿Cuál? ¿Por qué?
-3-¿Responde adecuadamente el programa a la pulsación de la tecla? 
-4-¿Qué pasa si el tiempo de cada led aumenta o disminuye? ¿Cómo responde la tecla, mejor o peor?
-
+se lo utiliza con puntero a funcion 
--- Clase2PM/src/app.c
+++ Clase2PM/src/app.c
@@ -5,73 +5,78 @@
  *===========================================================================*/
 
 // Inlcusiones
-
 #include "../../Clase2PM/inc/app.h"         // <= Su propia cabecera
 
 #include "../../Clase2PM/inc/board_ext.h"
 #include "../../Clase2PM/inc/key.h"
 #include "sapi.h"        // <= Biblioteca sAPI
 
-
 #define DUTY_CICLE 200
 
 /*=====[Encabezados de funciones]=============================================*/
 
-
-
 /*=====[Typedef]==============================================================*/\
-typedef enum{
-	PARADA = 0,
-	ADELANTE,
-	ATRAS,
-	TOTAL_ESTADOS
-}estado_secuencia;
+typedef enum {
+	PARADA = 0, ADELANTE, ATRAS, TOTAL_ESTADOS
+} estado_secuencia;
 
-typedef enum{
-	APAGAR,
-	PRENDER
-}estado_led;
-
+typedef enum {
+	APAGAR, PRENDER
+} estado_led;
 
 const uint8_t cantidad_leds = 3;	//indica la cantidad de led disponibles
 
 // FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE ENCENDIDO O RESET.
-int main( void )
-{
-   // ---------- CONFIGURACIONES ------------------------------
+int main(void) {
+	// ---------- CONFIGURACIONES ------------------------------
 
-   // Inicializar y configurar la plataforma
-   boardConfig();
+	// Inicializar y configurar la plataforma
+	boardConfig();
 
-   key_init();		//inicializacion del arreglo contenedor de los pulsadores a FALSE, es decir, no pulsados
+	key_init();	//inicializacion del arreglo contenedor de los pulsadores a FALSE, es decir, no pulsados
 
+	delay_t titilarLed1;
+	delayInit(&titilarLed1, 1200);
 
-   // ---------- REPETIR POR SIEMPRE --------------------------
+	delay_t titilarLed2;
+	delayInit(&titilarLed2, 500);
 
+	delay_t titilarLed3;
+	delayInit(&titilarLed3, 800);
 
-   while( TRUE ) {
+	// ---------- REPETIR POR SIEMPRE --------------------------
 
+	while ( TRUE) {
 
-	   key_periodicTask1ms();		//chequeo por pooling si un pulsador fue presionado
+		key_periodicTask1ms();//chequeo por pooling si un pulsador fue presionado
 
-	   if(key_getPressEv(BOARD_SW_ID_1)){	//si se pulsa el sw1 comienza secuencia para un lado
-		   board_setLed(BOARD_LED_ID_1, BOARD_LED_MSG_TOGGLE);
-	   }
+		if (key_getPressEv(BOARD_SW_ID_1)) {//si se pulsa el sw1 comienza secuencia para un lado
+			board_setLed(BOARD_LED_ID_1, BOARD_LED_MSG_TOGGLE);
+		}
 
+		if (delayRead(&titilarLed1)) {
+			board_setLed(BOARD_LED_ID_1, BOARD_LED_MSG_TOGGLE);
+		}
 
-	   delay(1);
+		if (delayRead(&titilarLed2)) {
+			board_setLed(BOARD_LED_ID_2, BOARD_LED_MSG_TOGGLE);
+		}
 
-   }
+		if (delayRead(&titilarLed3)) {
+			board_setLed(BOARD_LED_ID_3, BOARD_LED_MSG_TOGGLE);
+		}
 
-   // NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa se ejecuta
-   // directamenteno sobre un microcontroladore y no es llamado por ningun
-   // Sistema Operativo, como en el caso de un programa para PC.
-   return 0;
+		//delay(1);
+
+	}
+
+	// NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa se ejecuta
+	// directamenteno sobre un microcontroladore y no es llamado por ningun
+	// Sistema Operativo, como en el caso de un programa para PC.
+	return 0;
 }
 
 /**	@brief Funcion que prende los leds segun parametro de ingreso
  * 	@param[in]	led	Referencia al led a prender *
  */
-
-
 
